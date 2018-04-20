4b456a7b22ecf2a3e713531fdedc0ed6b6d09057
EmersonYe <emersonye@gmail.com>
2018-04-19 21:58:48 -0700

    Trying to split activity into fragments

--- app/src/main/java/edu/sjsu/emerson/alphafitness/RecordWorkoutPortraitFragment.java
+++ app/src/main/java/edu/sjsu/emerson/alphafitness/RecordWorkoutPortraitFragment.java
@@ -16,87 +16,89 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import static android.content.Context.BIND_AUTO_CREATE;
 
 public class RecordWorkoutPortraitFragment extends Fragment
-        implements
-        OnMapReadyCallback,
-        GoogleMap.OnMyLocationButtonClickListener
 {
-    /**
-     * Request code for location permission request.
-     *
-     * @see #onRequestPermissionsResult(int, String[], int[])
-     */
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
-
-    /**
-     * Flag indicating whether a requested permission has been denied after returning in
-     * {@link #onRequestPermissionsResult(int, String[], int[])}.
-     */
-    private boolean mPermissionDenied = false;
-
-    private GoogleMap mMap;
-    private Button mStartWorkoutButton;
-
+    MapView mMapView;
+    private GoogleMap googleMap;
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment =
-                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
+        View rootView = inflater.inflate(R.layout.fragment_record_workout_portrait, container, false);
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState)
-    {
-        View v = inflater.inflate(R.layout.fragment_record_workout_portrait, container, false);
+        mMapView = (MapView) rootView.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
 
-        mMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        mMapFragment.getMapAsync(this);
+        mMapView.onResume(); // needed to get the map to display immediately
 
-        return v;
-    }
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState)
-    {
-        super.onViewCreated(view, savedInstanceState);
-        mStartWorkoutButton = view.findViewById(R.id.button_start);
+        mMapView.getMapAsync(new OnMapReadyCallback()
+        {
+            @Override
+            public void onMapReady(GoogleMap mMap)
+            {
+                googleMap = mMap;
+
+                enableMyLocation();
+                // For showing a move to my location button
+                googleMap.setMyLocationEnabled(true);
+
+                // For dropping a marker at a point on the Map
+                LatLng sydney = new LatLng(-34, 151);
+                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
+
+        return rootView;
     }
 
     @Override
-    public boolean onMyLocationButtonClick()
+    public void onResume()
     {
-        Toast.makeText(getActivity(), "MyLocation button clicked", Toast.LENGTH_SHORT).show();
-        return false;
+        super.onResume();
+        mMapView.onResume();
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
-    public void onMapReady(GoogleMap googleMap)
+    public void onPause()
     {
-        mMap = googleMap;
+        super.onPause();
+        mMapView.onPause();
+    }
 
-        mMap.setOnMyLocationButtonClickListener(this);
-        enableMyLocation();
-        onMyLocationButtonClick();
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory()
+    {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 
     /**
@@ -114,5 +116,4 @@ public class RecordWorkoutPortraitFragment extends Fragment
             mMap.setMyLocationEnabled(true);
         }
     }
-
 }
