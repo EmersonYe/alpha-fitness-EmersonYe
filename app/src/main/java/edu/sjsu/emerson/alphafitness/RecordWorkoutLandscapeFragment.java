64fde429cf867d4ac00bb9ada38590843d7d2643
EmersonYe <emersonye@gmail.com>
2018-05-05 21:29:52 -0700

    First commit for fragment details

--- app/src/main/java/edu/sjsu/emerson/alphafitness/RecordWorkoutLandscapeFragment.java
+++ app/src/main/java/edu/sjsu/emerson/alphafitness/RecordWorkoutLandscapeFragment.java
@@ -1,6 +1,12 @@
 package edu.sjsu.emerson.alphafitness;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by emersonsjsu on 4/18/18.
@@ -8,4 +14,15 @@ import android.app.Fragment;
 
 public class RecordWorkoutLandscapeFragment extends Fragment
 {
+    TextView averageSpeedText, maxSpeedText, minSpeedText;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState)
+    {
+        View v = inflater.inflate(R.layout.fragment_details, container, false);
+        averageSpeedText = v.findViewById(R.id.averageSpeed);
+        maxSpeedText = v.findViewById(R.id.maxSpeed);
+        minSpeedText = v.findViewById(R.id.minSpeed);
+        return v;
+    }
 }
