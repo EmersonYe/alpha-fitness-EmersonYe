a2095d14d951de80faab6fbe7ab790db9e865aae
EmersonYe <emersonye@gmail.com>
2018-05-05 17:58:13 -0700

    Implement saving user entered data

--- app/src/main/java/edu/sjsu/emerson/alphafitness/ProfileFragment.java
+++ app/src/main/java/edu/sjsu/emerson/alphafitness/ProfileFragment.java
@@ -7,12 +7,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class ProfileFragment extends Fragment
 {
     private User mUser;
     private EditText nameField, genderField, weightField;
+    private Button updateButton;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState)
@@ -26,14 +28,31 @@ public class ProfileFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState)
     {
         View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        // Obtain reference to UI elements
         nameField = v.findViewById(R.id.nameField);
         genderField = v.findViewById(R.id.genderField);
         weightField = v.findViewById(R.id.weightField);
+        updateButton = v.findViewById(R.id.updateButton);
 
+        updateIdentityFields();
+
+        updateButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                mUser.setName(nameField.getText().toString());
+                mUser.setGender(genderField.getText().toString());
+                mUser.setWeight(Float.parseFloat(weightField.getText().toString()));
+            }
+        });
+
+        return v;
+    }
+
+    private void updateIdentityFields() {
         nameField.setText(mUser.getName());
         genderField.setText(mUser.getGender());
         weightField.setText(String.valueOf(mUser.getWeight()));
-
-        return v;
     }
 }
